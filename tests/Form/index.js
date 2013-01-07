b6a6daa9705b1531ce7fcf0c0d60d9b3578e76a7
tehfreak <tehfreak@tehfreak.com>
2013-01-07 22:39:12 +0300

    необходимы тесты

--- tests/Form/index.js
+++ tests/Form/index.js
@@ -4,7 +4,11 @@ module.exports= function (Form) {
 
     return function () {
         describe('Form — конструктор формы', function () {
-
+            it('should be instance of Function', function () {
+                assert.isFunction(
+                    Form
+                )
+            })
         })
     }
 
